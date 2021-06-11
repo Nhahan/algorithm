e6d500e9b36051919b70cc8947ef1c0c36ab8776
Nhahan <kisy324@naver.com>
2021-06-11 22:17:11 +0900

    week homework 1 & 2

---
+++ week_2/homework/01_get_kth_node_from_last.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def append(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def get_kth_node_from_last(self, k):
+        # 구현해보세요!
+        if k == 0:
+            return print("1 이상의 정수를 입력해주세요.")
+        cur = self.head
+        for i in range(k-1):
+            cur = cur.next
+        self.head = cur
+        return self.head
+
+
+linked_list = LinkedList(6)
+linked_list.append(7)
+linked_list.append(8)
+
+print(linked_list.get_kth_node_from_last(2).data)  # 7이 나와야 합니다!
---
+++ week_2/homework/02_is_available_to_order.py
@@ -0,0 +1,16 @@
+shop_menus = ["만두", "떡볶이", "오뎅", "사이다", "콜라"]
+shop_orders = ["오뎅", "콜라", "만두"]
+
+
+def is_available_to_order(menus, orders):
+    # 이 부분을 채워보세요!
+    for order in shop_orders:
+        if order in shop_menus:
+            continue
+        else:
+            return False
+    return True
+
+
+result = is_available_to_order(shop_menus, shop_orders)
+print(result)
