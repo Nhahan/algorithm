29bf53339f3d8adfea30f66ada4ce74e37c22a1d
Nhahan <kisy324@naver.com>
2021-06-11 20:12:37 +0900

    linked_list

---
+++ week_2/01_print_all_linked_list.py
@@ -0,0 +1,30 @@
+# [3] -> [4]
+# data, next
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+node = Node(3)
+first_node = Node(4)
+node.next = first_node
+
+class LinkedList:
+    def __init__(self, data):
+        self.head = Node(data)
+
+    def append(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+        self.head.next = Node(data)
+
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(data)
+
+linked_list = LinkedList(3)
+print(linked_list.head.data)
